111dacecd124361a14f1afb588f18c10d5f6d0c4
Antonio Iliev <iliev.t.a@gmail.com>
2018-09-19 14:33:37 +0300

    - LocalDataBase

---
+++ Towers of Hanoi/TowersOfHanoi/DataBase/LocalDataBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using TowersOfHanoi.Models;
+
+namespace TowersOfHanoi.DataBase
+{
+    public class LocalDataBase
+    {
+        private List<Step> steps;
+        private int diskCounts;
+
+        public int DiskCounts { get => diskCounts; set => diskCounts = value; }
+        public List<Step> Steps { get => new List<Step>(this.steps); set => steps = value; }
+    }
+}
