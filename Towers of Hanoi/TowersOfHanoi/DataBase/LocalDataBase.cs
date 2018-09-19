5bfd99660419ed1a28e92b41b6d5eb784982d296
Antonio Iliev <iliev.t.a@gmail.com>
2018-09-19 16:38:09 +0300

    - Solve when elements is even

--- Towers of Hanoi/TowersOfHanoi/DataBase/LocalDataBase.cs
+++ Towers of Hanoi/TowersOfHanoi/DataBase/LocalDataBase.cs
@@ -8,7 +8,8 @@ namespace TowersOfHanoi.DataBase
         private static List<Step> steps;
         private static int diskCounts;
 
+
         public static int DiskCounts { get => diskCounts; set => diskCounts = value; }
-        public static List<Step> Steps { get => new List<Step>(steps); set => steps = value; }
+        public static List<Step> Steps { get => new List<Step>(steps); set => steps = new List<Step>(); }
     }
 }
