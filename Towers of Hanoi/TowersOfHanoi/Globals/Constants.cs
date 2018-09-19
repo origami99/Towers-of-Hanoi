f3b9ad2b43cbce888da8234da179d7e5809720fb
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-19 15:33:45 +0300

    Set global constants

---
+++ Towers of Hanoi/TowersOfHanoi/Globals/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace TowersOfHanoi.Globals
+{
+    public class Constants
+    {
+        public const int CONSOLE_WIDTH = 30;
+
+        public const int CONSOLE_HEIGHT = 10;
+
+        public const int MIN_PEGS = 3;
+
+        public const int MAX_PEGS = 10;
+    }
+}
