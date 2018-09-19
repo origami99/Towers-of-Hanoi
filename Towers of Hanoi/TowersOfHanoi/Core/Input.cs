64e1d56131bcb4e64e4e7506f67cec61b81da20f
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-19 15:34:06 +0300

    Make the input validation

--- Towers of Hanoi/TowersOfHanoi/Core/Input.cs
+++ Towers of Hanoi/TowersOfHanoi/Core/Input.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using TowersOfHanoi.Globals;
 
 namespace TowersOfHanoi.Core
 {
     public class Input
     {
-        public int Get() => int.Parse(Console.ReadLine());
+        private int discsCount = 0;
+
+        public int DiscsCount
+        {
+            get
+            {
+                return this.discsCount;
+            }
+            private set
+            {
+                if (discsCount == 0)
+                    throw new ArgumentException($"Discs count is not set yet. Invoke the ReadInt() method first");
+
+                if (value > Constants.MAX_PEGS)
+                    throw new ArgumentException($"Input number must be between {Constants.MIN_PEGS} and {Constants.MAX_PEGS}. Currernt value is {value}");
+
+                this.discsCount = value;
+            }
+        }
+
+        public void ReadInt()
+        {
+            Console.WriteLine($"Input number between {Constants.MIN_PEGS} and {Constants.MAX_PEGS}:");
+
+            int n = int.Parse(Console.ReadLine());
+
+            this.DiscsCount = n;
+        }
     }
 }
